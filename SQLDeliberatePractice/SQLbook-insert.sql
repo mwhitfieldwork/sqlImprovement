2135d4a38654adcf81828d9c7c0275e4a7d1b131
mike whitfield <mwhitfieldwork@gmail.com>
2022-05-23 17:47:53 -0500

    Add Insert query

---
+++ SQLDeliberatePractice/SQLbook-insert.sql
@@ -0,0 +1,6 @@
+use Northwind
+go
+
+Insert into Employees
+      (LastName,FirstName,Title,TitleOfCourtesy,BirthDate,HireDate ,Address,City,Region,PostalCode,Country,HomePhone,Extension,Photo,Notes,ReportsTo,PhotoPath)
+values('Whitfield','Daphne','Little VP',null,'2012-4-13','2022-02-13','123 Circle St','Bellwood',null,'70012','United States','(900)888-7123',null,null,null,null,null)
