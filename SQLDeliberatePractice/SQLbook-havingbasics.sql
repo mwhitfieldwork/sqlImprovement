f508db024151d0564c847606292d5201b2a68b0d
mike whitfield <whitfieldwork@gmail.com>
2023-08-16 08:49:01 -0500

    Add more notes to the groupby and having sql query

--- SQLDeliberatePractice/SQLbook-havingbasics.sql
+++ SQLDeliberatePractice/SQLbook-havingbasics.sql
@@ -1,5 +1,18 @@
 -- Where AND Having
 --places the conditions on the grupps created by the group buy clause
+-- the wehre clasue is processed after the from clause and before the group clause
+-- the having is processed after the group clause
+
   select sum(unitPrice) as 'gross unit price'
   from products
   group by SupplierID Having supplierID = 1 order by SupplierID desc
+
+  -- we can use and aggragate function in the having but not the where, because the having is processed after the group by
+  -- the data sum the sum of the unti price is greater that 20
+  -- a where clause will use an index and a having will not
+    select sum(unitPrice) as 'gross unit price'
+  from products
+  group by SupplierID Having sum(unitPrice) > 50 order by SupplierID desc
+
+  -- WHEN to ue the where clause instead of a having...
+  -- If the filtering can happen without the having, then use the where clause - it imporves performance
