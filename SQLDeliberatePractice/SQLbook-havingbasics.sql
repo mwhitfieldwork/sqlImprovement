d7fd158d9ed2943f7f653200d409ba560672dbde
mike whitfield <whitfieldwork@gmail.com>
2023-08-16 08:36:24 -0500

    Add beeter definition for the having clause

--- SQLDeliberatePractice/SQLbook-havingbasics.sql
+++ SQLDeliberatePractice/SQLbook-havingbasics.sql
@@ -1,4 +1,5 @@
 -- Where AND Having
+--places the conditions on the grupps created by the group buy clause
   select sum(unitPrice) as 'gross unit price'
   from products
   group by SupplierID Having supplierID = 1 order by SupplierID desc
